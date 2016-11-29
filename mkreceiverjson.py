f8f472f61cd329a2786ee6e22a3e814bdfe71aaa
pyrog <pyrog@laposte.net>
2016-11-29 22:19:51 +0100

    Fix Issue #13
    Also, we could use threads.

--- mkreceiverjson.py
+++ mkreceiverjson.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import json
+import requests
+from PIL import Image
+from io import BytesIO
 from datetime import datetime
 from argparse import ArgumentParser
 
@@ -31,6 +34,11 @@ if __name__ == "__main__":
                         default=False,
                         action="store_true",
                         help="Obfuscate email addresses (truncate addresses after '@').")
+    PARSER.add_argument("--cache",
+                        dest="cache",
+                        default=False,
+                        action="store_true",
+                        help="Cache locally and resize images.")
     ARGS = PARSER.parse_args()
 
     print("Fetch and parse lists of receivers")
@@ -51,6 +59,32 @@ if __name__ == "__main__":
     receiverdb = {'version': RECEIVERLIST_VERSION,
                   'receivers': receivers,
                   'timestamp': timestamp.isoformat()}
+
+    if ARGS.cache:
+        print("Cache photos")
+        for receiver in receivers:
+            i = 1
+            for photo in receiver['photos']:
+                try:
+                    fn = "photos/"+receiver.get('callsign')+"-"+str(i)
+                    print(fn+" "+photo)
+                    req = requests.get(photo)
+                    im = Image.open(BytesIO(req.content))
+                    
+                    im.resize((1024,1024), Image.HAMMING)
+                    im.save(fn+".jpg", "JPEG")
+                    
+                    im.thumbnail((100,100))
+                    im.save(fn+"-100.jpg", "JPEG")
+                    
+                    # Update URL in JSON datas
+                    #receiver.get('photos')[i-1].update({'url': fn+".jpg"})
+                    receiver.get('photos')[i-1] = fn+".jpg"
+
+                except Exception as e:
+                    print(e)
+				    
+                i = i+1
 
     if ARGS.obfuscate:
         print("Obfuscate email addresses")
